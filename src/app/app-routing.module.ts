890b43f1658636a576556b64676296e770376be0
Felipe Tapias <atapias@sistecredito.com>
2023-08-02 16:33:06 -0500

    Proyecto base

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { GraficoBarrasComponent } from './grafico-barras/grafico-barras.component';
-import { GraficoLineasComponent } from './grafico-lineas/grafico-lineas.component';
 
-const routes: Routes = [{ path: 'barras', component: GraficoBarrasComponent, pathMatch: 'full' },
-                        { path: 'lineal', component: GraficoLineasComponent, pathMatch: 'full' }];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GraficoBarrasComponent } from './grafico-barras/grafico-barras.component';
-import { GraficoLineasComponent } from './grafico-lineas/grafico-lineas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GraficoBarrasComponent,
-    GraficoLineasComponent
   ],
   imports: [
     BrowserModule,
